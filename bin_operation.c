5e45981b842934a732b03a49b32cc2653bd4910f
Abdulkadir Yeşilkaya <abdulkadir7725@gmail.com>
2023-02-25 13:25:21 +0300

    binary sayılar düzenlendi ve yeni diziye aktarıldı

--- bin_operation.c
+++ bin_operation.c
@@ -8,6 +8,7 @@ int main(int argc, char *argv[]) {
 	// binary file reading and writing
 	
 	FILE *dd;
+	char bin[700];
 	char ad[20];
 	dd=fopen("./binary.txt","r");
 	if (dd == NULL)
@@ -17,16 +18,39 @@ int main(int argc, char *argv[]) {
 	}
 	else
 	{ 
+		int i =0;
 		while (!feof(dd))
 		{ 
 			fgets(ad, 8, dd);
-			printf("%s\n", ad);
+			
+			int x;
+			for(x = 0; x<7; x++)
+			{
+				bin[i] = ad[x];
+				i++;
+			}
+			
+			
 		}
 		
 	fclose(dd);
 	
 	}
-
+	
+	int j;
+	for(j=0; j<sizeof(bin); j=j+7)
+	{
+		int k;
+		for(k=j; k<j+7; k++)
+		{
+			printf("%c", bin[k]);
+		}
+		printf("\n");
+	}
+	
+	
+	
+	
 	
 	return 0;
 }
